d6d68265ef6f52b0d2cdd61a17dd0f314c17c4c9
JeongHwan Yoon <yoonjh8080@gmail.com>
2022-01-02 20:23:16 +0900

    ㅁ

--- selfStudy/src/timesTable.java
+++ selfStudy/src/timesTable.java
@@ -10,20 +10,20 @@ public class timesTable {
 		
 		int dan = 1;
 		int i = 1;
-		boolean temp = true;
+		boolean temp = true; //기본 전역함수 설정
 		
 		while (temp)
 		{
-			System.out.println(" *** " + dan +"단 ***");
+			System.out.println(" *** " + dan +"단 ***"); // 단출력
 			while(i <10)
 			{
-			System.out.println(dan +  " * " + i + " = " + dan * i);
+			System.out.println(dan +  " * " + i + " = " + dan * i); // 구구단값 출력
 			i++;
 			}
 			dan += 1;
 			i = 1;
 			
-			if (dan == 10)
+			if (dan > 9) // 종료값 (9단까지이므로 10설정)
 			{
 			temp = false;
 			}
