b6d9b31c42f1657c7992d6b3a77b58a242e53189
JeongHwan Yoon <yoonjh8080@gmail.com>
2022-01-02 19:22:22 +0900

    test

---
+++ selfStudy/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * 1~100까지 숫자의 합을 구하여 출력해보기
+		 */
+		
+		int sum = 0; //
+		
+		for (int i=1; i<=100; i++)
+		{
+			sum += i;
+		}
+		
+		System.out.println("1~100의 합 : " + sum);
+		
+	}
+
+}
